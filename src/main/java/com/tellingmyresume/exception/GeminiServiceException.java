45df26ee3fca65883e129652d3913344e9755b1a
Leonardo <leoferolive@gmail.com>
2025-08-30 04:16:52 -0300

    feat: extend GeminiServiceException to inherit from AIServiceException with provider information

--- src/main/java/com/tellingmyresume/exception/GeminiServiceException.java
+++ src/main/java/com/tellingmyresume/exception/GeminiServiceException.java
@@ -1,13 +1,14 @@
 package com.tellingmyresume.exception;
 
-public class GeminiServiceException extends RuntimeException {
-	private static final long serialVersionUID = 5706305532153892666L;
+public class GeminiServiceException extends AIServiceException {
+    private static final long serialVersionUID = 5706305532153892666L;
+    private static final String AI_PROVIDER = "Gemini";
 
-	public GeminiServiceException(String message) {
-        super(message);
+    public GeminiServiceException(String message) {
+        super(AI_PROVIDER, message);
     }
 
     public GeminiServiceException(String message, Throwable cause) {
-        super(message, cause);
+        super(AI_PROVIDER, message, cause);
     }
 }
